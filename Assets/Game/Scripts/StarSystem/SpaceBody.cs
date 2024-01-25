05591bba12e0618f2180816a46de5d2b543c7f28
tka2704 <tka2704@gmail.com>
2024-01-25 22:38:36 +0300

    simple generator

--- Assets/Game/Scripts/StarSystem/SpaceBody.cs
+++ Assets/Game/Scripts/StarSystem/SpaceBody.cs
@@ -6,7 +6,9 @@ namespace Game.Scripts.StarSystem
     [ExecuteInEditMode]
     public class SpaceBody : MonoBehaviour
     {
+        private static float _lastDistance = 1;
         [SerializeField] private SpaceBody parent;
+        [SerializeField] private SpaceBody[] satellites;
 
         [SerializeField, Range(-180, 180)] private float tilt;
         [SerializeField, Range(-180, 180)] private float orbitAngle;
@@ -23,6 +25,26 @@ namespace Game.Scripts.StarSystem
         [SerializeField] private Transform cachedTransform;
 
         public Vector3 Position => cachedTransform.position;
+
+        public void SetParentBody(SpaceBody parentBody)
+        {
+            parent = parentBody;
+        }
+
+        public void SetRandomValues()
+        {
+            tilt = Random.Range(-180, 180);
+            orbitAngle = Random.Range(-180, 180);
+            periapsis = Random.Range(0, 100);
+            apoapsis = Random.Range(0, 100);
+            dstMultiplier = _lastDistance;
+            _lastDistance += 0.1f;
+
+            orbitPerSecond = Random.Range(0f, 1f);
+            orbitState = Random.Range(0f, 1f);
+            selfRotationPerSecond = Random.Range(0f, 1f);
+            selfRotationState = Random.Range(0f, 1f);
+        }
 
         private void OnValidate()
         {
